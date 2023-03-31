9f57bab3ec9c176e816fe5211c37a15c3a4bf644
ADOUKO Franck Venance <fvadouko@live.fr>
2023-03-31 21:16:28 +0000

    feat: add the correct api

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,127 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface Currency {
+  id: string;
+  name: string;
+  symbol: string;
+}
+
+interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+}
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState<Currency>({
+    id: 'bitcoin',
+    name: 'Bitcoin',
+    symbol: 'BTC',
+  });
+  const [toCurrency, setToCurrency] = useState<Currency>({
+    id: 'eur',
+    name: 'Euro',
+    symbol: 'EUR',
+  });
+  const [exchangeRate, setExchangeRate] = useState<number>(0);
+  const [fromAmount, setFromAmount] = useState<string>('');
+  const [toAmount, setToAmount] = useState<string>('');
+
+  useEffect(() => {
+    const fetchExchangeRate = async () => {
+      const response = await axios.get(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${toCurrency.id}&ids=${fromCurrency.id}&sparkline=true&locale=en`
+      );
+      const coin = response.data[0] as Coin;
+      setExchangeRate(coin.current_price);
+    };
+
+    fetchExchangeRate();
+  }, [fromCurrency, toCurrency]);
+
+  const handleFromCurrencyChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const currencyId = event.target.value;
+    const currency = {
+      id: currencyId,
+      name: currencyId.toUpperCase(),
+      symbol: currencyId.toUpperCase(),
+    };
+    setFromCurrency(currency);
+  };
+
+  const handleToCurrencyChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const currencyId = event.target.value;
+    const currency = {
+      id: currencyId,
+      name: currencyId.toUpperCase(),
+      symbol: currencyId.toUpperCase(),
+    };
+    setToCurrency(currency);
+  };
+
+  const handleFromAmountChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const amount = event.target.value;
+    setFromAmount(amount);
+    setToAmount((parseFloat(amount) * exchangeRate).toString());
+  };
+
+  const handleToAmountChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const amount = event.target.value;
+    setToAmount(amount);
+    setFromAmount((parseFloat(amount) / exchangeRate).toString());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div>
+        <label>From:</label>
+        <select
+          value={fromCurrency.id}
+          onChange={handleFromCurrencyChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option value="bitcoin">BTC</option>
+          <option value="ethereum">ETH</option>
+          <option value="litecoin">LTC</option>
+        </select>
+        <input
+          type="number"
+          value={fromAmount}
+          onChange={handleFromAmountChange}
+        />
+      </div>
+      <div>
+        <label>To:</label>
+        <select
+          value={toCurrency.id}
+          onChange={handleToCurrencyChange}
+        >
+          <option value="usd">USD</option>
+          <option value="eur">EUR</option>
+          <option value="gbp">GBP</option>
+        </select>
+        <input
+          type="number"
+          value={toAmount}
+          onChange={handleToAmountChange}
+        />
+      </div>
+      <div>
+        <p>
+          1 {fromCurrency.symbol} = {exchangeRate} {toCurrency.symbol}
+        </p>
+      </div>
+    </>
   );
 }
 
